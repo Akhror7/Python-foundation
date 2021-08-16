82ee75bd58cbbebbc212fdff5592ab9fd446ac83
Akhror7 <ahror.abdullayev.2000@gmail.com>
2021-08-16 04:25:10 -0700

    bo'lmadida

--- sariqdev33.py
+++ sariqdev33.py
@@ -1,44 +1,44 @@
 import pickle
-# with open('data/fayl.txt') as file:
-#     f = file.read()
-#     f = f.rstrip()
-#     f = f.replace('\n', '')
-#     print('ustida qanaqa' in f)
-#
-#
-# with open('data/pi_million_digits.txt') as file:
-#     pi = file.read()
-# bdate = '01062000'
-# pi = pi.rstrip()
-# pi = pi.replace('\n', '')
-# pi = pi.replace(' ', '')
-# print( bdate in pi)
-#
-# PI = float(pi)
-#
-# with open('data/PI.txt', 'wb') as file:
-#     pickle.dump(PI, file)
-#
+with open('data/fayl.txt') as file:
+    f = file.read()
+    f = f.rstrip()
+    f = f.replace('\n', '')
+    print('ustida qanaqa' in f)
 
-# again = 1
-# while again:
-#     ism = input('Ismingizni kiritng>>>')
-#     familiya = input('Familayangizni kiritng>>>')
-#     millat = input('Millatingizni kiritng>>>')
-#     yosh = input('Yoshingizni kiritng>>>')
-#     date = f"{familiya} {ism} {yosh} yoshda, millati {millat}"
-#     with open('data/date.pkl','ab') as file:
-#         pickle.dump(date,file)
-#
-#     again = int(input('Yana ma\'lumot qo\'shasizmi?>>>ha(1)/yo\'q(0)'))
-#
-#
 
-# with open('data/date.pkl', 'rb') as file:
-#     print(pickle.load(file))
-#     print(pickle.load(file))
-#     print(pickle.load(file))
-#
+with open('data/pi_million_digits.txt') as file:
+    pi = file.read()
+bdate = '01062000'
+pi = pi.rstrip()
+pi = pi.replace('\n', '')
+pi = pi.replace(' ', '')
+print( bdate in pi)
+
+PI = float(pi)
+
+with open('data/PI.txt', 'wb') as file:
+    pickle.dump(PI, file)
+
+
+again = 1
+while again:
+    ism = input('Ismingizni kiritng>>>')
+    familiya = input('Familayangizni kiritng>>>')
+    millat = input('Millatingizni kiritng>>>')
+    yosh = input('Yoshingizni kiritng>>>')
+    date = f"{familiya} {ism} {yosh} yoshda, millati {millat}"
+    with open('data/date.pkl','ab') as file:
+        pickle.dump(date,file)
+
+    again = int(input('Yana ma\'lumot qo\'shasizmi?>>>ha(1)/yo\'q(0)'))
+
+
+
+with open('data/date.pkl', 'rb') as file:
+    print(pickle.load(file))
+    print(pickle.load(file))
+    print(pickle.load(file))
+
 
 
 
